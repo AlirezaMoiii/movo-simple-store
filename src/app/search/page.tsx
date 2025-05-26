9afb071f449495c7b8767eb0415324b50d8da668
Alireza <Alirezaapollo5000@gmail.com>
2025-05-26 17:43:48 +0330

    add a simple functionality to search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -9,33 +9,51 @@ import {
 
 import { products } from "../lib/placeholder-data";
 
-function page() {
+import { fetchSearchResults } from "../lib/data";
+import { Button } from "@/components/ui/button";
+
+type PageProps = Promise<{
+  searchParams?: {
+    q?: string;
+    page?: string;
+  };
+}>;
+
+async function page(props: PageProps) {
+  const { searchParams } = await props;
+
+  const query = searchParams?.q || "";
+  const page = searchParams?.page || "1";
+  const data = await fetchSearchResults(query);
+
+  console.log(data);
+
+  console.log(query);
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 my-4">
-      {products.map(pr => {
-
-        return(
+      <h1>Search results for: {query}</h1>
+      {data ? data.map((pr) => {
+        return (
           <Card key={pr.id} className="h-64">
-          <CardHeader>
-            <CardTitle>Product title</CardTitle>
-            <CardDescription>Product Description</CardDescription>
-          </CardHeader>
-              <CardContent className="relative w-36 h-36 z-10 flex items-center justify-center w-full">
-                {/* <Image
+            <CardHeader>
+              <CardTitle>{pr.name}</CardTitle>
+              <CardDescription>{pr.description}</CardDescription>
+            </CardHeader>
+            <CardContent className="relative w-36 h-36 z-10 flex items-center justify-center w-full">
+              {/* <Image
                   src={pr.images[0]}
                   fill
                   className="object-contain"
                   alt={`${pr.title} image`}
                 /> */}
-                Product Image
-              </CardContent>
-          <CardFooter>
-            <p>Product Footer (Buy - Like)</p>
-          </CardFooter>
-        </Card>
-        )
-      })}
-     
+              Product Image
+            </CardContent>
+            <CardFooter>
+               <Button>Add to card</Button>
+            </CardFooter>
+          </Card>
+        );
+      }) : <h1>We couldnt find anything</h1>}
     </div>
   );
 }
